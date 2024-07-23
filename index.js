1f0507ce1f3a1748530ebf7baf14281e14c1a649
Simba <jfrancis1195@gmail.com>
2024-07-23 19:03:31 -0400

    host on heroku

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ const Models = require('./models');
 const Movies = Models.Movie;
 const Users = Models.User;
 
-mongoose.connect('mongodb://127.0.0.1:27017/movie-api-db', { useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.connect('mongodb://127.0.0.1:27017/movie-api-db', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
 
 
 // Middleware definitions
