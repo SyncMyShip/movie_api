c8d7af91d2ceed4417e9a066daa28f3b2b1f8653
Simba <jfrancis1195@gmail.com>
2024-08-02 21:50:49 -0400

    revert auth edits

--- auth.js
+++ auth.js
@@ -1,7 +1,4 @@
 const jwtSecret = 'your_jwt_secret';
-const express = require("express");
-
-const app = express();
 
 const jwt = require('jsonwebtoken'),
     passport = require('passport');
@@ -15,9 +12,6 @@ let generateJWTToken = (user) => {
         algorithm: 'HS256'
     });
 }
-
-const cors = require('cors');
-app.use(cors()); // this syntax allows access from world
 
 // POST login
 module.exports = (router) => {
