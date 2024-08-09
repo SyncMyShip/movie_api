f528b9fc3fd08d0d00a54f5c6e546a28ba5bfb9a
Simba <jfrancis1195@gmail.com>
2024-08-08 23:10:58 -0400

    update signup path and jwtsecret definition

--- passport.js
+++ passport.js
@@ -42,7 +42,7 @@ passport.use(
 
 passport.use(new JWTStrategy({
     jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-    secretOrKey: 'your_jwt_secret'
+    jwtSecret: 'your_jwt_secret'
 }, async (jwtPayload, callback) => {
     return await Users.findById(jwtPayload._id)
         .then((user) => {
