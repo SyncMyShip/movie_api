134a726505a39aca0d48d33d4195f2e29fce3af9
Simba <jfrancis1195@gmail.com>
2024-08-29 21:44:49 -0400

    fix origin allowlist

--- index.js
+++ index.js
@@ -37,7 +37,8 @@ let allowedOrigins = [
     'http://localhost:1234',
     'http://localhost:8080', 
     'https://reelvouz.netlify.app', 
-    'https://deploy-preview-13--reelvouz.netlify.app/'
+    'https://deploy-preview-13--reelvouz.netlify.app',
+    'https://reelvouz.netlify.app'
 ] // TODO: whitelist frontend app
 
 app.use(cors({
